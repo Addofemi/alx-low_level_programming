dfa45bc078559fb5cc741c0df2e1ae8df346644c
Addofemi <femiaddo@gmail.com>
2023-02-19 14:30:38 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: A program that prints the size of a various types of computers
+ * Return; returns 0
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+printf("size of a char: %ld byte(s)\n", (unsigned long)sizeof(a));
+printf("size of an int: %ld byte(s)\n", (unsigned long)sizeof(b));
+printf("size of a long int: %ld byte(s)\n", (unsigned long)sizeof(c));
+printf("size of a long long int: %ld byte(s)\n", (unsigned long)sizeof(d));
+printf("size of a float: %ld byte(s)\n", (unsigned long)sizeof(e));
+return (0);
+}
